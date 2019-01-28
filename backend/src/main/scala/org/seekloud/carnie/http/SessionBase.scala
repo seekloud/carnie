aa008293c12a8da31678c86a96ba420589745402
dongruoyan <dongruoyan@neotel.com.cn>
2019-01-28 12:11:35 +0800

    LICENSE

--- backend/src/main/scala/org/seekloud/carnie/http/SessionBase.scala
+++ backend/src/main/scala/org/seekloud/carnie/http/SessionBase.scala
@@ -1,3 +1,17 @@
+//  Copyright 2018 seekloud (https://github.com/seekloud)
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package org.seekloud.carnie.http
 
 /**
@@ -71,8 +85,7 @@ object SessionBase extends CirceSupport{
 
 }
 
-trait
-SessionBase extends SessionSupport{
+trait SessionBase extends SessionSupport{
 
   import akka.http.scaladsl.server
   import akka.http.scaladsl.server.Directives.extractRequestContext
